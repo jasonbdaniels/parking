0c2cb2d7bb4203f2bdacaa0d7ca37e89b257d562
jasonbdaniels <jasonbdaniels@gmail.com>
2016-11-09 13:33:13 -0500

    Updates getinfo endpoint with json content type. Issue #2

--- getinfo/index.php
+++ getinfo/index.php
@@ -1,6 +1,8 @@
 <?php
 	include_once("../parking_util_functions.php");
 	
+	header("Content-Type: application/json");
+	
 	if(!isset($_GET['parkplaceid'])){
 		echo createInvalidMessage("Require parkplaceid");
 		exit();
