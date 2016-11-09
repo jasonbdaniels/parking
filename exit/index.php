8c5912ee3c17bc3176dc6ef8a05857e10256d49b
jasonbdaniels <jasonbdaniels@gmail.com>
2016-11-09 13:29:50 -0500

    Updates enter endpoint with json content type. Issue #2

--- exit/index.php
+++ exit/index.php
@@ -1,6 +1,8 @@
 <?php
 	include_once("../parking_util_functions.php");
 	
+	header("Content-Type: application/json");
+	
 	if(!isset($_POST['parkplaceid'])){
 		echo createInvalidMessage("Require parkplaceid");
 		exit();
